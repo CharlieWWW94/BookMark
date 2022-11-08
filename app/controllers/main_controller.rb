630d203db753838580cdf6e9e8b2425cf3934a62
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-08 21:29:36 +0000

    Allow users to add book search results to their favourites list through active record many-to-many

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,6 +5,8 @@ class MainController < ApplicationController
     if params[:searchTerm]
       foundBooks = BookGetter.new(params[:searchTerm])
       @booksDisplay = foundBooks.res["docs"]
+      favourites = List.where("title = ? AND user_id = ?", "Favourites", current_user.id)
+      @f_id = favourites[0].id
     end
     @currentURL = root_path
   end
